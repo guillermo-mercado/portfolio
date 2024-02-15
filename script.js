cdd8c0caec3a9448ea92806ef865273f02cb6577
guillermo-mercado <150946772+guillermo-mercado@users.noreply.github.com>
2024-02-15 17:31:54 +0000

    Recent Projects animation MOUSEOVER/MOUSELEAVE

--- script.js
+++ script.js
@@ -24,3 +24,35 @@ function changeBg() {
     }
 }
 window.addEventListener('scroll', changeBg)
+
+// --------Recent Projects animation MOUSEOVER/MOUSELEAVE--------
+var ex1 = document.querySelector(".ex1")
+var ex2 = document.querySelector(".ex2")
+var ex3 = document.querySelector(".ex3")
+
+ex1.addEventListener('mouseenter', (event) => {
+    ex2.classList.add('projectEffect1');
+    ex3.classList.add('projectEffect1');
+})
+ex1.addEventListener("mouseleave", (e) => {
+    ex2.classList.remove("projectEffect1")
+    ex3.classList.remove('projectEffect1');
+});
+
+ex2.addEventListener('mouseenter', (event) => {
+    ex1.classList.add('projectEffect1');
+    ex3.classList.add('projectEffect1');
+})
+ex2.addEventListener("mouseleave", (e) => {
+    ex1.classList.remove("projectEffect1")
+    ex3.classList.remove('projectEffect1');
+});
+
+ex3.addEventListener('mouseenter', (event) => {
+    ex1.classList.add('projectEffect1');
+    ex2.classList.add('projectEffect1');
+})
+ex3.addEventListener("mouseleave", (e) => {
+    ex1.classList.remove("projectEffect1")
+    ex2.classList.remove('projectEffect1');
+});
